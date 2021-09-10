bafd0e7fc181d18ff7d33ede5ec00d88edcbf032
lancer82 <lp_job@163.com>
2021-09-10 08:48:32 +0800

    Update compare and output report

--- src/test/java/compare/Students2Compare.java
+++ src/test/java/compare/Students2Compare.java
@@ -1,13 +1,12 @@
 package compare;
 
 import com.github.crab2died.ExcelUtils;
-import modules.Student1;
+import com.github.crab2died.exceptions.Excel4JException;
 import modules.Student2;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -22,10 +21,9 @@ public class Students2Compare {
 
     private static List<Student2> stu2List;
     private static List<Student2> stu2sList;
-    String tempPath = "/student2_map_template.xlsx";
-
-
-
+    static String  tempPath = "/student2_map_template.xlsx";
+    private static final List<Student2> class2 = new ArrayList<>();
+    private static final List<Student2> class4 = new ArrayList<>();
 
     @BeforeClass
     public static void ReadCsvForTheTest() {
@@ -44,23 +42,25 @@ public class Students2Compare {
     public void compareSpecific() throws Exception{
         ListIterator<Student2> it = stu2List.listIterator();
         List<Student2> student2List;
-        List<Student2> class2 = new ArrayList<>();
-        List<Student2> class4 = new ArrayList<>();
-        Map<String, List<?>> classes = new HashMap<>();
-        Map<String, String> data = new HashMap<>();
-        data.put("title", "战争学院花名册");
-        data.put("info", "学校统一花名册");
+
         while(it.hasNext()){
             Student2 stu = it.next();
             Long id = stu.getId();
             student2List = stu2sList.stream().filter(stu2 -> stu2.getId().equals(id)).collect(Collectors.toList());
-
             if (student2List.size() == 1) {
                 class2.add(new Student2(stu.getId(),stu.getName(),stu.getDate(),stu.getClasses(),stu.isExpel()));
             } else {
                 class4.add(new Student2(stu.getId(),stu.getName(),stu.getDate(),stu.getClasses(),stu.isExpel()));
             }
         }
+    }
+
+    @AfterClass
+    public static void tearDown() throws Excel4JException {
+        Map<String, List<?>> classes = new HashMap<>();
+        Map<String, String> data = new HashMap<>();
+        data.put("title", "战争学院花名册");
+        data.put("info", "学校统一花名册");
         classes.put("class_two", Arrays.asList(class2.toArray()));
         classes.put("class_four",Arrays.asList(class4.toArray()));
         ExcelUtils.getInstance().exportMap2Excel(tempPath,
